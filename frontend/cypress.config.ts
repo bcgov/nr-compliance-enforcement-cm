232a622b7266f3b9f74b7c05e6b6f55dd09d470a
barrfalk <bfalk@salussystems.com>
2023-10-24 15:12:51 -0700

    Initial commit

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://quickstart-openshift-test-frontend.apps.silver.devops.gov.bc.ca/',
+    baseUrl: 'https://nr-compliance-enforcement-cm-test-frontend.apps.silver.devops.gov.bc.ca/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
