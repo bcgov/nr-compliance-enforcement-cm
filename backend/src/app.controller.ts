d5f2ee2f262486863490a1b62342fd73cc25217a
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2024-01-23 13:29:39 -0800

    CE-308

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller({
+  path: "",
+  version: "1",
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/get-hello')
   getHello(): string {
+    console.log("in get-hello");
     return this.appService.getHello();
   }
 }
