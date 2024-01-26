6fe6f04e5a0815312932aeae54c4d537a635d419
Mike Sears <msears@parcsystems.ca>
2024-01-26 15:58:08 -0800

    update to audit column names
    update the create_ and update_ columns to include the utc identifier

--- migrations/sql/V1.1.0__CE-39.sql
+++ migrations/sql/V1.1.0__CE-39.sql
@@ -10,10 +10,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_sexcode" PRIMARY KEY (sex_code)
   );
 
@@ -29,10 +29,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_agecode" PRIMARY KEY (age_code)
   );
 
@@ -48,10 +48,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_thrtlvlcd" PRIMARY KEY (threat_level_code)
   );
 
@@ -67,10 +67,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_cnfthistcd" PRIMARY KEY (conflict_history_code)
   );
 
@@ -86,10 +86,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_earcode" PRIMARY KEY (ear_code)
   );
 
@@ -105,10 +105,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_drugcode" PRIMARY KEY (drug_code)
   );
 
@@ -124,10 +124,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_drgmethdcd" PRIMARY KEY (drug_method_code)
   );
 
@@ -143,10 +143,10 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_drgrmotccd" PRIMARY KEY (drug_remaining_outcome_code)
   );
 
@@ -162,17 +162,17 @@ CREATE TABLE
     active_ind bool NOT NULL,
     create_user_id varchar(32) NOT NULL,
     create_user_guid uuid NULL,
-    create_timestamp timestamp NOT NULL,
+    create_utc_timestamp timestamp NOT NULL,
     update_user_id varchar(32) NOT NULL,
     update_user_guid uuid NULL,
-    update_timestamp timestamp NOT NULL,
+    update_utc_timestamp timestamp NOT NULL,
     CONSTRAINT "PK_hwcrotcmcd" PRIMARY KEY (hwcr_outcome_code)
   );
 
 --
 -- INSERT INTO sex_code
 --
-insert into sex_code (sex_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into sex_code (sex_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('M', 'Male', 'Male', 1, true, user, null, now(), user, null, now()),
       ('F', 'Female', 'Female', 2, true, user, null, now(), user, null, now()),
       ('U', 'Unknown', 'Unknown', 3, true, user, null, now(), user, null, now());
@@ -181,7 +181,7 @@ values('M', 'Male', 'Male', 1, true, user, null, now(), user, null, now()),
 --
 -- INSERT INTO age_code
 --
-insert into age_code (age_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into age_code (age_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('ADLT', 'Adult', 'Adult', 1, true, user, null, now(), user, null, now()),
       ('YRLN', 'Yearling', 'Yearling', 2, true, user, null, now(), user, null, now()),
       ('YOFY', 'Young of the year', 'Young of the year', 3, true, user, null, now(), user, null, now()),
@@ -190,7 +190,7 @@ values('ADLT', 'Adult', 'Adult', 1, true, user, null, now(), user, null, now()),
 --
 -- INSERT INTO threat_level_code
 --
-insert into threat_level_code (threat_level_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into threat_level_code (threat_level_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('1', 'Category 1', 'Category 1', 1, true, user, null, now(), user, null, now()),
       ('2', 'Category 2', 'Category 2', 2, true, user, null, now(), user, null, now()),
       ('3', 'Category 3', 'Category 3', 3, true, user, null, now(), user, null, now()),
@@ -199,7 +199,7 @@ values('1', 'Category 1', 'Category 1', 1, true, user, null, now(), user, null, 
 --
 -- INSERT INTO conflict_history_code
 --
-insert into conflict_history_code (conflict_history_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into conflict_history_code (conflict_history_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('L', 'Low', 'Low', 1, true, user, null, now(), user, null, now()),
       ('M', 'Medium', 'Medium', 2, true, user, null, now(), user, null, now()),
       ('H', 'High', 'High', 3, true, user, null, now(), user, null, now()),
@@ -208,14 +208,14 @@ values('L', 'Low', 'Low', 1, true, user, null, now(), user, null, now()),
 --
 -- INSERT INTO ear_code
 --
-insert into ear_code (ear_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into ear_code (ear_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('L', 'Left', 'Left', 1, true, user, null, now(), user, null, now()),
       ('R', 'Right', 'Right', 2, true, user, null, now(), user, null, now());
 
 --
 -- INSERT INTO drug_code
 --
-insert into drug_code (drug_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into drug_code (drug_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('ATPMZ', 'Atipamezole', 'Atipamezole', 1, true, user, null, now(), user, null, now()),
       ('BAMII', 'BAM II', 'Butorphanol Azaperone Medetomidine', 2, true, user, null, now(), user, null, now()),
       ('MDTMD', 'Medetomidine', 'Medetomidine', 3, true, user, null, now(), user, null, now()),
@@ -225,7 +225,7 @@ values('ATPMZ', 'Atipamezole', 'Atipamezole', 1, true, user, null, now(), user, 
 --
 -- INSERT INTO drug_method_code
 --
-insert into drug_method_code (drug_method_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into drug_method_code (drug_method_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('DART', 'Dart', 'Dart', 1, true, user, null, now(), user, null, now()),
       ('HINJ', 'Hand injection', 'Hand injection', 2, true, user, null, now(), user, null, now()),
       ('PSRG', 'Pole syringe', 'Pole syringe', 3, true, user, null, now(), user, null, now());
@@ -233,7 +233,7 @@ values('DART', 'Dart', 'Dart', 1, true, user, null, now(), user, null, now()),
 --
 -- INSERT INTO drug_remaining_outcome_code
 --
-insert into drug_remaining_outcome_code (drug_remaining_outcome_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into drug_remaining_outcome_code (drug_remaining_outcome_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('DISC', 'Male', 'Male', 1, true, user, null, now(), user, null, now()),
       ('STOR', 'Female', 'Female', 2, true, user, null, now(), user, null, now()),
       ('RDIS', 'Unknown', 'Unknown', 3, true, user, null, now(), user, null, now());
@@ -242,7 +242,7 @@ values('DISC', 'Male', 'Male', 1, true, user, null, now(), user, null, now()),
 --
 -- INSERT INTO hwcr_outcome_code
 --
-insert into hwcr_outcome_code (hwcr_outcome_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_timestamp, update_user_id, update_user_guid, update_timestamp)
+insert into hwcr_outcome_code (hwcr_outcome_code, short_description, long_description, display_order, active_ind, create_user_id, create_user_guid, create_utc_timestamp, update_user_id, update_user_guid, update_utc_timestamp)
 values('DEADONARR', 'Dead on arrival', '', 1, true, user, null, now(), user, null, now()),
       ('DESTRYCOS', 'Destroyed by COS', '', 2, true, user, null, now(), user, null, now()),
       ('DESTRYOTH', 'Destroyed by other', '', 3, true, user, null, now(), user, null, now()),
