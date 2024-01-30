2775367637fe7e1c62cc67bf62b85a16f2273311
barrfalk <bfalk@salussystems.com>
2024-01-29 16:02:34 -0800

    prisma updates

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from 'nestjs-prisma';
 
 @Module({
   providers: [PrismaService],
