15c1f8cb487e64b2ded5c1d218105639f996a2df
barrfalk <bfalk@salussystems.com>
2024-01-29 17:14:40 -0800

    Import fixes

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { PrismaModule } from '../../src/prisma/prisma.module';
 import { UsersResolver } from './users.resolver';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [PrismaModule],
