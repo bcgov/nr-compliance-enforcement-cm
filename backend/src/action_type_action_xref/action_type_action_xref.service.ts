675d771efbd2cbb88d9784b6b89f9abd24eb8281
Dmitri Korin <dmitri.korin@gov.bc.ca>
2024-03-01 13:50:00 -0800

    Add description to action codes

--- backend/src/action_type_action_xref/action_type_action_xref.service.ts
+++ backend/src/action_type_action_xref/action_type_action_xref.service.ts
@@ -1,14 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from "nestjs-prisma";
+import { ActionTypeActionXref } from './entities/action_type_action_xref.entity';
 
 @Injectable()
 export class ActionTypeActionXrefService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
-  find(action_type_code? : string) {
+  async find(action_type_code?: string) {
     const dataContext = this.prisma.action_type_action_xref;
-    return action_type_code ? dataContext.findMany( { where: { action_type_code: action_type_code } })
-                       : dataContext.findMany() ;
-                      
+    let queryResult = null;
+
+    if (action_type_code) {
+      queryResult = await dataContext.findMany({
+        where: {
+          action_type_code: action_type_code
+        },
+        select: {
+          action_type_code: true,
+          action_code: true,
+          display_order: true,
+          active_ind: true,
+          action_code_action_type_action_xref_action_codeToaction_code: {
+            select: {
+              short_description: true,
+              long_description: true,
+            }
+          }
+        }
+      })
+    }
+    else {
+      queryResult = await dataContext.findMany();
+    }
+    let actionCodes: Array<ActionTypeActionXref> = [];
+    for (const record of queryResult) {
+      actionCodes.push({
+        action_type_code: record.action_type_code,
+        action_code: record.action_code,
+        display_order: record.display_order,
+        active_ind: record.active_ind,
+        short_description: record.action_code_action_type_action_xref_action_codeToaction_code.short_description,
+        long_description: record.action_code_action_type_action_xref_action_codeToaction_code.long_description
+      });
+    }
+    return actionCodes;
   }
 }
--- backend/src/action_type_action_xref/entities/action_type_action_xref.entity.ts
+++ backend/src/action_type_action_xref/entities/action_type_action_xref.entity.ts
@@ -1,1 +1,8 @@
-export class ActionTypeActionXref {}
+export class ActionTypeActionXref {
+    action_type_code: string
+    action_code: string
+    display_order: number
+    active_ind: boolean 
+    short_description: string
+    long_description: string
+}
