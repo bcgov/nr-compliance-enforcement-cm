877c9f95b62b8a0fc68704620d29f7e32caa52c9
Scarlett <35635257+Scarlett-Truong@users.noreply.github.com>
2024-04-08 16:21:06 -0700

    feat: update/save File Review (#40)
    Co-authored-by: cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
    Co-authored-by: barrfalk <bfalk@salussystems.com>
    Co-authored-by: afwilcox <alecwilcox@gmail.com>

---
+++ backend/src/case_file/dto/review-input.ts
@@ -0,0 +1,14 @@
+export class ReviewInput {
+  leadIdentifier: string;
+  agencyCode: string;
+  caseCode: string;
+  userId: string;
+  isReviewRequired: boolean;
+  caseIdentifier?: string;
+  reviewComplete?: {
+    actor: string;
+    date: Date;
+    actionCode: string;
+    actionId?: string;
+  }
+}
