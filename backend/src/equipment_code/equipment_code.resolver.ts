ce80fe2fa21a3edbc3e1edd0b2d16f41108aea4b
cnesmithsalus <69365683+cnesmithsalus@users.noreply.github.com>
2024-04-29 13:27:21 -0700

    copy paste error

--- backend/src/equipment_code/equipment_code.resolver.ts
+++ backend/src/equipment_code/equipment_code.resolver.ts
@@ -1,11 +1,12 @@
 
 import { Resolver, Query, } from '@nestjs/graphql';
 import { ActionCodeService } from '../action_code/action_code.service';
-import { JwtRoleGuard } from "../auth/jwtrole.guard";
-import { UseGuards } from "@nestjs/common";
 import { Role } from "../enum/role.enum";
-import { Roles } from "../auth/decorators/roles.decorator";hGuard } from "../auth/jwtauth.guard";
+import { Roles } from "../auth/decorators/roles.decorator";
 import { ACTION_TYPE_CODES } from '../common/action_type_codes';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwtauth.guard';
+import { JwtRoleGuard } from '../auth/jwtrole.guard';
 
 @UseGuards(JwtAuthGuard, JwtRoleGuard)
 @Resolver("EquipmentCode")
