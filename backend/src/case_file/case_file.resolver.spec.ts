b984a993a077ba7466263dd5d22318130a17e91d
barrfalk <bfalk@salussystems.com>
2024-05-07 16:39:37 -0700

    Update case_file.resolver.spec.ts

--- backend/src/case_file/case_file.resolver.spec.ts
+++ backend/src/case_file/case_file.resolver.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { CaseFileResolver } from "./case_file.resolver";
 import { CaseFileService } from "./case_file.service";
 import { PrismaModule } from "nestjs-prisma";
+import { CaseFileActionService } from "../case_file_action/case_file_action.service";
 
 describe("CaseFileResolver", () => {
   let resolver: CaseFileResolver;
@@ -9,7 +10,14 @@ describe("CaseFileResolver", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [PrismaModule],
-      providers: [CaseFileResolver, CaseFileService],
+      providers: [
+        CaseFileResolver,
+        CaseFileService,
+        {
+          provide: CaseFileActionService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     resolver = module.get<CaseFileResolver>(CaseFileResolver);
