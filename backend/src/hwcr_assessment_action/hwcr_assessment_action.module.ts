195a764a5b397f081d842f6ccfb8e0a8ec03eb8f
afwilcox <alecwilcox@gmail.com>
2024-05-13 10:12:19 -0700

    Cleanup some dead code

--- backend/src/hwcr_assessment_action/hwcr_assessment_action.module.ts
+++ backend/src/hwcr_assessment_action/hwcr_assessment_action.module.ts
@@ -1,8 +1,7 @@
-
-import { Module } from '@nestjs/common';
-import { PrismaModule } from 'nestjs-prisma';
-import { HWCRAssessmentActionResolver } from './hwcr_assessment_action.resolver';
-import { ActionCodeService } from '../action_code/action_code.service';
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "nestjs-prisma";
+import { HWCRAssessmentActionResolver } from "./hwcr_assessment_action.resolver";
+import { ActionCodeService } from "../action_code/action_code.service";
 
 @Module({
   imports: [PrismaModule],
