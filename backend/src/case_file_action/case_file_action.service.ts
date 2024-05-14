1443a6d506c1ac2701f6d17d7dcc37bb52ad1dbb
afwilcox <alecwilcox@gmail.com>
2024-05-14 09:24:02 -0700

    Added try/catch and query consolidation

--- backend/src/case_file_action/case_file_action.service.ts
+++ backend/src/case_file_action/case_file_action.service.ts
@@ -2,107 +2,85 @@ import { Injectable, Logger } from "@nestjs/common";
 import { PrismaService } from "nestjs-prisma";
 import { CaseFileAction } from "./entities/case_file_action.entity";
 import { ACTION_TYPE_CODES } from "../common/action_type_codes";
+import { GraphQLError } from "graphql";
 
 @Injectable()
 export class CaseFileActionService {
   constructor(private prisma: PrismaService) {}
   private readonly logger = new Logger(CaseFileActionService.name);
 
-  async findActionById(actionId: string) {
-    const actionContext = this.prisma.action;
-    const actionCodeContext = this.prisma.action_code;
-
-    let action = await actionContext.findFirst({
-      where: {
+  //helper method that is used to map the Prisma result set into a useable object
+  private mapActionResult(actionResult: any) {
+    if (actionResult) {
+      const {
         action_guid: actionId,
-      },
-      select: {
-        action_guid: true,
-        actor_guid: true,
-        active_ind: true,
-        action_type_action_xref: true,
-        action_date: true,
-      },
-    });
-
-    let actionCode = await actionCodeContext.findFirst({
-      where: {
-        action_code: action.action_type_action_xref.action_code,
-      },
-      select: {
-        short_description: true,
-        long_description: true,
-      },
-    });
-
-    const caseFileAction = Object.assign(new CaseFileAction(), {
-      actionId: action.action_guid,
-      actor: action.actor_guid,
-      activeIndicator: action.active_ind,
-      actionCode: action.action_type_action_xref.action_code,
-      shortDescription: actionCode.short_description,
-      longDescription: actionCode.long_description,
-      actionTypeCode: action.action_type_action_xref.action_type_code,
-      displayOrder: action.action_type_action_xref.display_order,
-      date: action.action_date,
-    });
-
-    return caseFileAction;
-  }
-
-  async findActionByXrefIdAndCaseId(caseId: string, actionXrefGuid: string) {
-    const actionContext = this.prisma.action;
-    const actionCodeContext = this.prisma.action_code;
-    let caseFileAction: CaseFileAction = {
-      actionId: "",
-      actor: "",
-      activeIndicator: false,
-      actionCode: "",
-      shortDescription: "",
-      longDescription: "",
-      actionTypeCode: "",
-      displayOrder: 0,
-      date: null,
-    };
-
-    let queryResult = await actionContext.findFirst({
-      where: {
-        case_guid: caseId,
-        action_type_action_xref_guid: actionXrefGuid,
-      },
-      select: {
-        action_guid: true,
-        actor_guid: true,
-        active_ind: true,
-        action_type_action_xref: true,
-        action_date: true,
-      },
-    });
-    if (queryResult) {
-      let actionCode = await actionCodeContext.findFirst({
-        where: {
-          action_code: queryResult.action_type_action_xref.action_code,
+        actor_guid: actor,
+        active_ind: activeIndicator,
+        action_date: date,
+        action_type_action_xref: {
+          action_type_code: actionTypeCode,
+          display_order: displayOrder,
+          action_code_action_type_action_xref_action_codeToaction_code: {
+            action_code: actionCode,
+            short_description: shortDescription,
+            long_description: longDescription,
+          },
         },
-        select: {
-          short_description: true,
-          long_description: true,
-        },
-      });
-      caseFileAction.actionId = queryResult.action_guid;
+      } = actionResult;
 
-      caseFileAction.actionId = queryResult.action_guid;
-      caseFileAction.actor = queryResult.actor_guid;
-      caseFileAction.activeIndicator = queryResult.active_ind;
-      caseFileAction.actionCode = queryResult.action_type_action_xref.action_code;
-      caseFileAction.shortDescription = actionCode.short_description;
-      caseFileAction.longDescription = actionCode.long_description;
-      caseFileAction.actionTypeCode = queryResult.action_type_action_xref.action_type_code;
-      caseFileAction.displayOrder = queryResult.action_type_action_xref.display_order;
-      caseFileAction.date = queryResult.action_date;
-      return caseFileAction;
+      return {
+        actionId,
+        actor,
+        activeIndicator,
+        date,
+        actionTypeCode,
+        displayOrder,
+        actionCode,
+        shortDescription,
+        longDescription,
+      };
     }
   }
 
+  //helper method that is used to find the details of all the details of an action given a case and actionType/actionCode pair.
+  //This could potentially be refactored by combining it with findActionsByCaseIdAndType
+  private async findActionByXrefIdAndCaseId(caseId: string, actionXrefGuid: string) {
+    const actionContext = this.prisma.action;
+
+    try {
+      let actionResult = await actionContext.findFirst({
+        where: {
+          case_guid: caseId,
+          action_type_action_xref_guid: actionXrefGuid,
+        },
+        select: {
+          action_guid: true,
+          actor_guid: true,
+          active_ind: true,
+          action_type_action_xref: {
+            select: {
+              action_type_code: true,
+              display_order: true,
+              action_code_action_type_action_xref_action_codeToaction_code: {
+                select: {
+                  action_code: true,
+                  short_description: true,
+                  long_description: true,
+                },
+              },
+            },
+          },
+          action_date: true,
+        },
+      });
+
+      return this.mapActionResult(actionResult);
+    } catch (exception) {
+      throw new GraphQLError("Exception occurred. See server log for details", {});
+    }
+  }
+
+  //Used to return the all the actions of a given type for a specific case
   async findActionsByCaseIdAndType(caseId: string, actionTypeCode: string) {
     const actionCodeXrefContext = this.prisma.action_type_action_xref;
 
@@ -126,61 +104,44 @@ export class CaseFileActionService {
     return caseFileActions;
   }
 
+  //Used to return the all the actions of a given code for a specific case
   async findActionByCaseIdAndCaseCode(caseId: string, actionCaseCode: string) {
     const actionContext = this.prisma.action;
-    const actionCodeContext = this.prisma.action_code;
-    const actionCodeXrefContext = this.prisma.action_type_action_xref;
 
-    let xrefResult = await actionCodeXrefContext.findFirst({
-      where: {
-        action_type_code: ACTION_TYPE_CODES.CASEACTION,
-        action_code: actionCaseCode,
-      },
-      select: {
-        action_type_action_xref_guid: true,
-      },
-    });
-
-    let caseFileAction;
-    if (xrefResult) {
-      const queryResult = await actionContext.findFirst({
+    try {
+      const actionResult = await actionContext.findFirst({
         where: {
           case_guid: caseId,
-          action_type_action_xref_guid: xrefResult.action_type_action_xref_guid,
+          action_type_action_xref: {
+            action_code_action_type_action_xref_action_codeToaction_code: {
+              action_code: actionCaseCode,
+            },
+          },
         },
         select: {
           action_guid: true,
           actor_guid: true,
           active_ind: true,
-          action_type_action_xref: true,
+          action_type_action_xref: {
+            select: {
+              action_type_code: true,
+              display_order: true,
+              action_code_action_type_action_xref_action_codeToaction_code: {
+                select: {
+                  action_code: true,
+                  short_description: true,
+                  long_description: true,
+                },
+              },
+            },
+          },
           action_date: true,
         },
       });
 
-      if (queryResult) {
-        let actionCode = await actionCodeContext.findFirst({
-          where: {
-            action_code: queryResult.action_type_action_xref.action_code,
-          },
-          select: {
-            short_description: true,
-            long_description: true,
-          },
-        });
-        caseFileAction = Object.assign({
-          actionId: queryResult.action_guid,
-          actor: queryResult.actor_guid,
-          activeIndicator: queryResult.active_ind,
-          actionCode: queryResult.action_type_action_xref.action_code,
-          shortDescription: actionCode.short_description,
-          longDescription: actionCode.long_description,
-          actionTypeCode: queryResult.action_type_action_xref.action_type_code,
-          displayOrder: queryResult.action_type_action_xref.display_order,
-          date: queryResult.action_date,
-        });
-      }
+      return this.mapActionResult(actionResult);
+    } catch (exception) {
+      throw new GraphQLError("Exception occurred. See server log for details", {});
     }
-
-    return caseFileAction;
   }
 }
