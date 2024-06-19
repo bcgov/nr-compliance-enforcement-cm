e91d20943b17f9be1a46cf2110e3d42f50285fff
gregorylavery <100631366+gregorylavery@users.noreply.github.com>
2024-06-19 13:07:51 -0700

    Updated SQL to change the ordering and upate display_order for each current entry

--- migrations/sql/V1.20.1__CE-742.sql
+++ migrations/sql/V1.20.1__CE-742.sql
@@ -18,13 +18,64 @@ values
     'LESSLETHAL',
     'Less lethal',
     'Less lethal',
-    9,
+    50,
     true,
     'FLYWAY',
     now (),
     'FLYWAY',
     now ()
   );
+
+--
+-- UPDATE the display_order to allow future inserts to hwcr_outcome_code
+--
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 10
+WHERE
+  hwcr_outcome_code = 'DEADONARR';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 20
+WHERE
+  hwcr_outcome_code = 'DESTRYCOS';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 30
+WHERE
+  hwcr_outcome_code = 'DESTRYOTH';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 40
+WHERE
+  hwcr_outcome_code = 'GONEONARR';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 60
+WHERE
+  hwcr_outcome_code = 'REFRTOBIO';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 70
+WHERE
+  hwcr_outcome_code = 'SHRTRELOC';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 80
+WHERE
+  hwcr_outcome_code = 'TRANSLCTD';
+
+UPDATE case_management.hwcr_outcome_code
+SET
+  display_order = 90
+WHERE
+  hwcr_outcome_code = 'TRANSREHB';
 
 UPDATE case_management.configuration
 SET
