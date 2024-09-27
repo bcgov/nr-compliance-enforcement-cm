232fa159ce78c2bd1e84482faf66e1404dd2516f
gregorylavery <100631366+gregorylavery@users.noreply.github.com>
2024-09-27 09:01:57 -0700

    Renamed the file and removed the extraneous version statement.

--- migrations/sql/V1.28.2__CE-1054.sql
+++ migrations/sql/V1.28.2__CE-1054.sql
@@ -11,9 +11,3 @@ DELETE from case_management.schedule_sector_xref;
 -- end PURGE of decision data
 -- Add uniqueness constraint to avoid repeatable seed data script from filling the DB with copies
 ALTER TABLE case_management.schedule_sector_xref ADD CONSTRAINT UK_schedule_sector_xref__schedule_sector UNIQUE (schedule_code, sector_code);
-
-UPDATE case_management.configuration
-SET
-  configuration_value = cast(configuration_value as INTEGER) + 1
-WHERE
-  configuration_code = 'CDTABLEVER';
