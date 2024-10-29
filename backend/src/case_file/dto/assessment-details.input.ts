ab18cd416f894789633e9d24519d749012bdd1f0
Mike Vesprini <11034827+mikevespi@users.noreply.github.com>
2024-10-29 15:27:08 -0700

    feat: Add actionLinkedComplaintIdentifier to AssessmentDetailsInput (#97)

--- backend/src/case_file/dto/assessment-details.input.ts
+++ backend/src/case_file/dto/assessment-details.input.ts
@@ -3,4 +3,6 @@ export class AssessmentDetailsInput {
   actionNotRequired: boolean;
   actionJustificationCode: string;
   actions: [AssessmentActionInput];
+  actionLinkedComplaintIdentifier: string;
+  actionCloseComplaint: boolean;
 }
