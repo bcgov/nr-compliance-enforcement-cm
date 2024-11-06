86c1083975630b9d0ae808791ae266f8b9e7941e
Josh Gamache <jsgamache@gmail.com>
2024-11-06 15:15:09 -0700

    chore: migrate into additional_comments fields

--- migrations/sql/V1.30.0__CE-556.sql
+++ migrations/sql/V1.30.0__CE-556.sql
@@ -11,3 +11,43 @@ UPDATE case_management.drug_remaining_outcome_code
   SET short_description='Returned to vet', long_description='Returned to vet', update_user_id='FLYWAY', update_utc_timestamp=now()
   WHERE drug_remaining_outcome_code='RDIS';
 
+--
+-- CREATE TABLE drug_additional_comments
+--
+
+ALTER TABLE case_management.drug_administered
+  ADD additional_comments_text varchar(1000) NULL;
+
+comment on column case_management.drug_administered.additional_comments_text is 'Includes comments on immobilization outcomes, any adverse reactions, and drug storage or discarding.';
+
+--
+-- Migrate existing adverse reactions, discard method, amount discarded to Additional comments
+--
+
+UPDATE case_management.drug_administered
+  SET additional_comments_text = TRIM(
+    CONCAT(
+      CASE WHEN adverse_reaction_text IS NOT NULL AND adverse_reaction_text <> ''
+        THEN 'Adverse reaction: ' || adverse_reaction_text || '; '
+        ELSE ''
+      END,
+      CASE WHEN discard_method_text IS NOT NULL AND discard_method_text <> ''
+        THEN 'Discard method: ' || discard_method_text || '; '
+        ELSE ''
+      END,
+      CASE WHEN drug_discarded_amount IS NOT NULL
+        THEN 'Amount discarded: ' || drug_discarded_amount::text || '; '
+        ELSE ''
+      END
+    )
+  )
+  WHERE additional_comments_text IS NULL;
+
+
+--
+-- DROP Adverse reactions, discard method, amount discarded columns.
+--
+
+ALTER TABLE case_management.drug_administered DROP COLUMN adverse_reaction_text;
+ALTER TABLE case_management.drug_administered DROP COLUMN drug_discarded_amount;
+ALTER TABLE case_management.drug_administered DROP COLUMN discard_method_text;
