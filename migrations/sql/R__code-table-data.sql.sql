3dad7f0473d462d57da6aa8dc70116948e5a6da1
gregorylavery <100631366+gregorylavery@users.noreply.github.com>
2024-11-22 11:46:31 -0800

    fix: Adds new entries to table. (#104)
    Co-authored-by: afwilcox <alecwilcox@gmail.com>

--- migrations/sql/R__code-table-data.sql.sql
+++ migrations/sql/R__code-table-data.sql.sql
@@ -5411,6 +5411,59 @@ SET
 WHERE
   drug_remaining_outcome_code = 'RDIS';
 
+--
+-- INSERT INTO hwcr_outcome_code
+--
+insert into
+  hwcr_outcome_code (
+    hwcr_outcome_code,
+    short_description,
+    long_description,
+    display_order,
+    active_ind,
+    create_user_id,
+    create_utc_timestamp,
+    update_user_id,
+    update_utc_timestamp
+  )
+values
+  (
+    'EUTHCOS',
+    'Euthanized by COS',
+    'Euthanized by COS',
+    15,
+    true,
+    'FLYWAY',
+    now (),
+    'FLYWAY',
+    now ()
+  ),
+  (
+    'EUTHOTH',
+    'Euthanized by Other',
+    'Euthanized by Other',
+    25,
+    true,
+    'FLYWAY',
+    now (),
+    'FLYWAY',
+    now ()
+  ) on conflict do nothing;
+
+update case_management.hwcr_outcome_code
+set
+  short_description = 'Dispatched by COS',
+  long_description = 'Dispatched by COS'
+where
+  hwcr_outcome_code = 'DESTRYCOS';
+
+update case_management.hwcr_outcome_code
+set
+  short_description = 'Dispatched by Other',
+  long_description = 'Dispatched by Other'
+where
+  hwcr_outcome_code = 'DESTRYOTH';
+
 --------------------------
 -- New Changes above this line
 -------------------------
