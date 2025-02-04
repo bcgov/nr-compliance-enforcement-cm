934336f875008e41a5843dd53fed6aea09aedd27
Ryan Rondeau <ryan_rondeau@live.ca>
2025-02-04 13:11:40 -0800

    Update V1.36.0__CE-1326.sql

--- migrations/sql/V1.36.0__CE-1326.sql
+++ migrations/sql/V1.36.0__CE-1326.sql
@@ -9,3 +9,9 @@ SET configuration_value = cast(configuration_value as INTEGER)+1 WHERE configura
 
 comment on column case_management.equipment.quantity is 'Indicates the number of EQUIPMENT used. Values should be a positive integer or null if not applicable. ';
 comment on column case_management.equipment_code.has_quantity_ind is 'Indicates if the type of equipment allows a quantity to be speciofied.   Default to ''false''. ';
+
+UPDATE case_management.equipment SET quantity = 1 
+WHERE 
+  active_ind = true 
+AND 
+  equipment_code IN ('FTRAP', 'SNR', 'SIGNG', 'TRCAM', 'LTRAP');
