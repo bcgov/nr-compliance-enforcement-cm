ef1f5586b08458eb6c1e5935f92609412cf27a74
afwilcox <alecwilcox@gmail.com>
2025-03-06 12:18:15 -0800

    feat: Release Mountain Beaver (#134)
    Co-authored-by: jon-funk <johnthomasfunk@gmail.com>
    Co-authored-by: Dmitri <108112696+dk-bcps@users.noreply.github.com>

--- backend/src/case_file/entities/prevention.entity.ts
+++
@@ -1,5 +0,0 @@
-import { CaseFileAction } from "../../case_file_action/entities/case_file_action.entity";
-
-export class Prevention {
-    actions: CaseFileAction[];
-}
---
+++ backend/src/investigation/temp_poc/entities/temp_poc.entity.ts
@@ -0,0 +1,7 @@
+import { Decimal } from "@prisma/client/runtime/library";
+
+export class TempPoc {
+  id: Decimal;
+  first_name: string;
+  last_name: string;
+}
