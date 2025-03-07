667957be49a24abedd1ce1d2dcbdaaf156786e01
Ryan Rondeau <ryan_rondeau@live.ca>
2025-03-07 15:29:56 -0800

    replace MUI with Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,45 +1,25 @@
-import Box from '@mui/material/Box'
 import AppRoutes from '@/routes'
 import { BrowserRouter } from 'react-router-dom'
 import { Footer, Header } from '@bcgov/design-system-react-components'
-import IconButton from '@mui/material/IconButton'
-import HomeIcon from '@mui/icons-material/Home'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  content: {
-    flexGrow: 1,
-    marginTop: '5em',
-    marginRight: '1em',
-    marginLeft: '1em',
-    marginBottom: '5em',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-  },
-}
 export default function App() {
   return (
-    <Box sx={styles.container}>
+    <Container>
       <Header title={'QuickStart OpenShift'}>
         {' '}
         <a href={'/'} target={'_self'}>
-          <IconButton color="secondary">
-            <HomeIcon color="secondary"></HomeIcon>
-          </IconButton>
+          <Button color="secondary">Home</Button>
         </a>
       </Header>
-      <Box sx={styles.content}>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </Box>
+      <Row>
+        <Col>
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </Col>
+      </Row>
       <Footer />
-    </Box>
+    </Container>
   )
 }
