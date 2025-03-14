ac26d5e3e3803c6b55e7766cf52717293c76011c
afwilcox <alecwilcox@gmail.com>
2025-03-14 15:35:06 -0700

    feat: remove PoC add automapper

--- backend/src/shared/contact_method/entities/contact_method.entity.ts
+++
@@ -1,6 +0,0 @@
-export class ContactMethod {
-  typeCode: string;
-  typeShortDescription: string;
-  typeDescription: string;
-  value: string;
-}
--- backend/src/shared/temp_poc/entities/temp_poc.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Decimal } from "@prisma/client/runtime/library";
-
-export class TempPoc {
-  id: Decimal;
-  first_name: string;
-  last_name: string;
-}
