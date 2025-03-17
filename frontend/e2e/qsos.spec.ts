52e92df195220f1cea83fb471400448de2a8f8c0
Mike Vesprini <mike@button.is>
2025-03-17 12:14:48 -0700

    Add Playwright auth with sample tests

--- frontend/e2e/qsos.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test'
-import { dashboard_page } from './pages/dashboard'
-
-test.describe.parallel('QSOS', () => {
-  test('Dashboard Page', async ({ page }) => {
-    await dashboard_page(page)
-  })
-})
