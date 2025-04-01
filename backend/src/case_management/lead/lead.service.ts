692c25408fbf559f51d651a9ffa42af8c5e0e0f3
Scarlett <35635257+Scarlett-Truong@users.noreply.github.com>
2025-04-01 09:26:24 -0700

    feat: CE-1362 add equipment filters (#140)

--- backend/src/case_management/lead/lead.service.ts
+++ backend/src/case_management/lead/lead.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { CaseManagementPrismaService } from "../../prisma/cm/prisma.cm.service";
 import { ACTION_TYPE_CODES } from "../../common/action_type_codes";
 import { ACTION_CODES } from "../../common/action_codes";
+import { EQUIPMENT_STATUS_CODES } from "../../common/equipment_status_codes";
 
 @Injectable()
 export class LeadService {
@@ -115,6 +116,130 @@ export class LeadService {
       where: {
         case_identifier: {
           in: caseGuids,
+        },
+      },
+      select: {
+        lead_identifier: true,
+      },
+    });
+
+    const leadIdentifiers: string[] = [];
+    for (let leadId of leadResults) {
+      leadIdentifiers.push(leadId.lead_identifier);
+    }
+
+    return leadIdentifiers;
+  }
+
+  async getLeadsByEquipment(equipmentStatus: string, equipmentCodes: string[]): Promise<string[]> {
+    const EXCEPTIONAL_INACTIVE_EQUIPMENT_CODES = ["K9UNT", "LLTHL"]; //K9 and Less lethal are always inactive equipments
+    const xrefResult = await this.prisma.action_type_action_xref.findMany({
+      where: {
+        OR: [{ action_code: ACTION_CODES.SETEQUIPMT }, { action_code: ACTION_CODES.REMEQUIPMT }],
+      },
+      select: {
+        action_type_action_xref_guid: true,
+      },
+      orderBy: {
+        action_code: "desc",
+      },
+    });
+
+    const setEquipmentActionGuid = xrefResult[0].action_type_action_xref_guid;
+    const removeEquipmentActionGuid = xrefResult[1].action_type_action_xref_guid;
+
+    //Find all cases with "set equipment" actions
+    const setActions = await this.prisma.action.findMany({
+      where: {
+        action_type_action_xref_guid: setEquipmentActionGuid,
+        equipment: {
+          active_ind: true, //exclude deleted equipments (active_ind = false)
+        },
+        active_ind: true,
+      },
+      select: {
+        case_guid: true,
+        equipment_guid: true,
+        equipment: {
+          select: {
+            equipment_code: true,
+          },
+        },
+      },
+    });
+
+    //Find all cases with "remove equipment" actions
+    const removeActions = await this.prisma.action.findMany({
+      where: {
+        action_type_action_xref_guid: removeEquipmentActionGuid,
+        equipment: {
+          active_ind: true,
+        },
+        active_ind: true,
+      },
+      select: {
+        case_guid: true,
+      },
+    });
+
+    //Determine active and inactive cases
+    const setCaseGuids = new Set(setActions.map((action) => action.case_guid));
+    const removeCaseGuids = new Set(removeActions.map((action) => action.case_guid));
+
+    const activeCaseGuids = new Set<string>();
+    const inactiveCaseGuids = new Set<string>();
+
+    setActions.forEach((action) => {
+      const caseGuid = action.case_guid;
+      const equipmentCode = action.equipment?.equipment_code;
+      // Check if the equipment type is an exceptional inactive code
+      const isExceptionalInactive = equipmentCode && EXCEPTIONAL_INACTIVE_EQUIPMENT_CODES.includes(equipmentCode);
+      if (removeCaseGuids.has(caseGuid) || isExceptionalInactive) {
+        inactiveCaseGuids.add(caseGuid);
+      } else {
+        activeCaseGuids.add(caseGuid);
+      }
+    });
+
+    //Filter cases based on equipmentStatus
+    let targetCaseGuids: string[];
+    switch (equipmentStatus) {
+      case EQUIPMENT_STATUS_CODES.ALL_EQUIPMENT:
+        targetCaseGuids = Array.from(setCaseGuids);
+        break;
+      case EQUIPMENT_STATUS_CODES.ACTIVE_EQUIPMENT:
+        targetCaseGuids = Array.from(activeCaseGuids);
+        break;
+      case EQUIPMENT_STATUS_CODES.INACTIVE_EQUIPMENT:
+        targetCaseGuids = Array.from(inactiveCaseGuids);
+        break;
+      default:
+        throw new Error("Invalid equipment status");
+    }
+
+    if (Array.isArray(equipmentCodes) && equipmentCodes.length > 0) {
+      const filteredActions = await this.prisma.action.findMany({
+        where: {
+          case_guid: { in: targetCaseGuids },
+          action_type_action_xref_guid: setEquipmentActionGuid,
+          equipment: {
+            equipment_code: { in: equipmentCodes },
+            active_ind: true,
+          },
+        },
+        select: {
+          case_guid: true,
+        },
+      });
+
+      targetCaseGuids = filteredActions.map((action) => action.case_guid);
+    }
+
+    //Return lead id
+    const leadResults = await this.prisma.lead.findMany({
+      where: {
+        case_identifier: {
+          in: targetCaseGuids,
         },
       },
       select: {
