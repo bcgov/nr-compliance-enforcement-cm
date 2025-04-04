c0496849b23e65fc79f0011387c0ac4f096b4992
Ryan Rondeau <ryan_rondeau@live.ca>
2025-04-04 16:18:48 -0700

    Create R__park-test-data.sql

---
+++ migrations/test-only-migrations/shared/R__park-test-data.sql
@@ -0,0 +1,8 @@
+insert into shared.park ("external_id", "name", "create_user_id", "update_user_id") 
+values ('1', 'Strathcona Park - Lindsay Loop', 'system', 'system');
+insert into shared.park ("external_id", "name", "create_user_id", "update_user_id") 
+values ('10', 'Inonoaklin Park', 'system', 'system');
+insert into shared.park ("external_id", "name", "create_user_id", "update_user_id") 
+values ('1000', 'Hunwadi/Ahnuhati-Bald Conservancy', 'system', 'system');
+insert into shared.park ("external_id", "name", "create_user_id", "update_user_id") 
+values ('73', 'sx̌ʷəx̌ʷnitkʷ Park', 'system', 'system');
