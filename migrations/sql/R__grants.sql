58bc0c04e1356fa28f14ab79e0a53fd0f512c22c
Ryan Rondeau <ryan_rondeau@live.ca>
2025-04-08 17:36:32 -0700

    Update R__grants.sql

--- migrations/sql/R__grants.sql
+++ migrations/sql/R__grants.sql
@@ -8,6 +8,8 @@ GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA shared TO proxy_js_shared;
 
 -- Grant future privileges to case management objects
 ALTER DEFAULT PRIVILEGES IN SCHEMA case_management GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO case_management;
+ALTER DEFAULT PRIVILEGES IN SCHEMA case_management GRANT USAGE, SELECT ON SEQUENCES TO case_management;
 
 -- Grant future privileges to shared schema objects
 ALTER DEFAULT PRIVILEGES IN SCHEMA shared GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO proxy_js_shared;
+ALTER DEFAULT PRIVILEGES IN SCHEMA shared GRANT USAGE, SELECT ON SEQUENCES TO proxy_js_shared;
