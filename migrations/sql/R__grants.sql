2ff678cb7e6684e924077c67cc6076e1fb446ebb
Ryan Rondeau <ryan_rondeau@live.ca>
2025-04-08 17:31:01 -0700

    Update R__grants.sql

--- migrations/sql/R__grants.sql
+++ migrations/sql/R__grants.sql
@@ -2,6 +2,12 @@
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA case_management TO case_management;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA case_management TO case_management;
 
--- Grant privileges to investigation objects
+-- Grant privileges to shared schema objects
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA shared TO proxy_js_shared;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA shared TO proxy_js_shared;
+
+-- Grant future privileges to case management objects
+ALTER DEFAULT PRIVILEGES IN SCHEMA case_management GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO case_management;
+
+-- Grant future privileges to shared schema objects
+ALTER DEFAULT PRIVILEGES IN SCHEMA shared GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO proxy_js_shared;
