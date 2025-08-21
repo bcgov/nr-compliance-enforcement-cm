e612fa7e09bd73b878775eafc0d28bbe02cff7bb
Mike Vesprini <mike@button.is>
2025-08-21 15:52:04 -0700

    Add createInvestigation fucntion

--- backend/src/investigation/investigation/investigation.module.ts
+++ backend/src/investigation/investigation/investigation.module.ts
@@ -4,9 +4,10 @@ import { InvestigationResolver } from "./investigation.resolver";
 import { PrismaModuleInvestigation } from "../../prisma/investigation/prisma.investigation.module";
 import { PrismaModuleShared } from "../../prisma/shared/prisma.shared.module";
 import { AutomapperModule } from "@automapper/nestjs";
+import { UserModule } from "../../common/user.module";
 
 @Module({
-  imports: [PrismaModuleInvestigation, PrismaModuleShared, AutomapperModule],
+  imports: [PrismaModuleInvestigation, PrismaModuleShared, AutomapperModule, UserModule],
   providers: [InvestigationResolver, InvestigationService],
 })
 export class InvestigationModule {}
